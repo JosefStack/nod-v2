9568e4de57ccca0803849cd2d019c1c9a6850d9c
JosefStack <jojohnson350@gmail.com>
2026-03-28 20:32:09 +0200

    setup authentication and auth middleware (protect)

---
+++ server/src/middleware/auth.middleware.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { prisma } from "../lib/prisma.ts";
+import { NetConnectOpts } from "node:net";
+
+export interface AuthRequest extends Request {
+    user?: {
+        id: string;
+        email: string;
+        username: string | null;
+        isOnboarded: boolean;
+    }
+}
+
+export const protect = async (req: AuthRequest, res: Response, next: NextFunction) => {
+
+    try {
+
+        const token = req.cookies.jwt;
+
+        if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { userId: string };
+
+        const user = await prisma.user.findUnique({
+            where: { id: decoded.userId },
+            select: {id: true, email: true, username: true, isOnboarded: true },
+        });
+
+        if (!user) return res.status(401).json({ message: "Unauthorized - User not found" });
+
+        req.user = user;
+        next();
+
+    } catch (err) {
+        console.error(err);
+        res.status(401).json({ message: "Unauthorized - Invalid token" });
+    }
+
+
+}
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { signup, login, logout } from "../controllers/authController.ts";
+import { signup, login, logout } from "../controllers/auth.controller.ts";
+import { Response } from "express";
+import { protect } from "../middleware/auth.middleware.ts";
+import { AuthRequest } from "../middleware/auth.middleware.ts";
 
 const authRouter = Router();
 
 authRouter.post("/signup", signup);
 authRouter.post("/login", login);
 authRouter.post("/logout", logout);
+
+authRouter.get("/check", protect, (req: AuthRequest, res: Response) => res.status(200).json(req.user))
 
 export default authRouter;
