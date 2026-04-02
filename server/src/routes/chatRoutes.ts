0fc7eb25abc1a170b401458f54fa4d6ee1f40c16
JosefStack <jojohnson350@gmail.com>
2026-04-02 11:38:54 +0300

    chat routes refactoring

--- server/src/routes/chatRoutes.ts
+++ server/src/routes/chatRoutes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { Response } from "express";
 import { protect } from "../middleware/auth.middleware.ts";
 import { AuthRequest } from "../middleware/auth.middleware.ts";
-import { getAllChats, getAllRooms, deleteDirectChat, deleteGroupChat, deleteRoomChat } from "../controllers/chat.controller.ts"
+import { getAllChats, getAllRooms } from "../controllers/chat.controller.ts"
 
 
 const chatRouter = Router();
