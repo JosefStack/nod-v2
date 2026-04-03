b55ad1d84cf66c7b2b6010cd7a408eb9ca13d131
JosefStack <jojohnson350@gmail.com>
2026-04-03 12:08:27 +0300

    implemented sendMessage controller

--- server/src/controllers/message.controller.ts
+++ server/src/controllers/message.controller.ts
@@ -1,6 +1,7 @@
 import { Response } from "express";
 import { AuthRequest } from "../middleware/auth.middleware.ts";
 import { prisma } from "../lib/prisma.ts";
+import cloudinary from "../lib/cloudinary.ts";
 
 
 
@@ -16,11 +17,11 @@ export const getAllMessages = async (req: AuthRequest, res: Response) => {
 
         if (!type) return res.json(400).json({ message: "type query param required" });
 
-        const where = 
+        const where =
             type === "direct" ? { directChatId: chatId } :
-            type === "group" ? { groupChatId: chatId } :
-            type === "room" ? { roomChatId: chatId } :
-            null;
+                type === "group" ? { groupChatId: chatId } :
+                    type === "room" ? { roomChatId: chatId } :
+                        null;
 
         if (!where) return res.status(400).json({ message: "Invalid chat type" });
 
@@ -32,7 +33,7 @@ export const getAllMessages = async (req: AuthRequest, res: Response) => {
                     select: {
                         id: true, username: true, avatar: true, name: true
                     }
-                }, 
+                },
                 attachments: true,
                 readBy: {
                     select: { userId: true }
@@ -48,12 +49,123 @@ export const getAllMessages = async (req: AuthRequest, res: Response) => {
     }
 }
 
+const verifyMembership = async (userId: string, chatId: string, chatType: string) {
+    if (chatType === "direct") {
+        const participant = await prisma.directChatParticipants.findFirst({
+            where: {
+                chatId, userId
+            }
+        });
+        return !!participant;
+    }
 
-export const sendMessage = (req: AuthRequest, res: Response) => {}
+    if (chatType === "group") {
+        const participant = await prisma.groupMember.findFirst({
+            where: {
+                chatId, userId
+            }
+        });
+        return !!participant;
+    }
+
+    if (chatType === "room") {
+        const participant = await prisma.roomMember.findFirst({
+            where: {
+                chatId, userId
+            }
+        });
+        return !!participant;
+    }
+};
+
+
+export const sendMessage = async (req: AuthRequest, res: Response) => {
+
+    try {
+
+        const userId = req.user?.id;
+        if (!userId) return res.status(401).json({ message: "Unauthorized" });
+
+        const { content, attachments, chatType, chatId } = req.body;
+
+        if (!content.trim() && (!attachments || attachments.length === 0)) {
+            return res.status(400).json({ message: "Message cannot be empty" });
+        }
+
+        if (!chatId || !chatType) {
+            return res.status(400).json({ message: "chatId and chatType is are requried" });
+        }
+
+        // check if sender (user) is a member of the provided chat
+        const isMember = await verifyMembership(userId, chatId, chatType);
+        if (!isMember) return res.status(403).json({ message: "You are not a part of this chat" });
+
+        let uplaodedAttachments: any[] = [];
+        if (attachments && attachments.length > 0) {
+            const upload = await Promise.all(
+                attachments.map((attachment: { data: string, type: string, fileName?: string, size?: number }) => {
+                    const result = await cloudinary.uploader.upload(attachment.data, {
+                        folder: attachment.type === "video" ? "nod/videos" : attachment.type === "image" ? "nod/images" : "nod/files",
+                        resource_type: attachment.type === "video" ? "video" : attachment.type === "image" ? "image" : "raw",
+                    });
+
+                    return {
+                        url: result.secure_url,
+                        type: attachment.type,
+                        fiNename: attachment.fileName || null,
+                        size: attachment.size || null,
+                        width: result.width || null,
+                        height: result.height || null,
+                        duration: result.duration || null,
+
+                    };
+                })
+            );
+
+            uplaodedAttachments = upload;
+        }
+
+        const chatRelation =
+            chatType === "direct" ? { directChatId: chatId } :
+                chatType === "group" ? { groupId: chatId } :
+                    chatType === "room" ? { roomId: chatId } :
+                        null;
+
+        if (!chatRelation) return res.status(400).json({ message: "Invalid chat type" });
+
+        //  content, attachments, chatType, chatId
+        const message = await prisma.message.create({
+            data: {
+                content: content?.trim() || null,
+                senderId: userId,
+                ...chatRelation,
+                attachments: {
+                    create: uplaodedAttachments
+                }
+            }, 
+            include: {
+                sender: {
+                    select: { id: true, name: true, avatar: true, username: true }
+                }, 
+                attachments: true
+            }
+        });
+
+        return res.json(201).json(message);
+
+
+    } catch (err: any) {
+        console.log("Failed to send message: ", err)
+        return res.status(500).json({ message: "Failed to send message" })
+    }
+
+
+}
 
 
 
 
 
 
-export const deleteMessage = (req: AuthRequest, res: Response) => {}
+
+export const deleteMessage = (req: AuthRequest, res: Response) => { }
