ea58838de3c486fac71a073fc13a0139e3d351a9
JosefStack <jojohnson350@gmail.com>
2026-04-03 13:53:14 +0300

    implemeted searchUsers controller

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -82,3 +82,38 @@ export const checkUsername = async (req: AuthRequest, res: Response) => {
 
 
 };
+
+export const searchUsers = async (req: AuthRequest, res: Response) => {
+    try {   
+
+        const userId = req.user?.id;
+        if (!userId) return res.status(401).json({ message: "Unauthorized" });
+
+        const { searchKey } = req.params;
+        if (!searchKey || typeof searchKey !== "string") return res.status(400).json({ message: "Search query required" });
+        if (searchKey.trim().length < 2) return res.status(400).json({ message: "Search query must have at least 2 characters" });
+
+        const users = await prisma.user.findMany({
+            where: {
+                AND: [
+                    { id: { not: userId } },
+                    {
+                        OR: [
+                            { username: { contains: searchKey, mode: "insensitive" } }, 
+                            { name: { contains: searchKey, mode: "insensitive" } },
+                        ]
+                    }
+                ]
+            }, select: {
+                id: true, username: true, name: true, avatar: true
+            }, 
+            take: 10,
+        });
+
+        return res.status(200).json(users);
+
+    } catch (err: any) {
+        console.error("User search failed: ", err);
+        return res.status(500).json({ message: "User search failed" })
+    }
+}
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -2,11 +2,15 @@ import { Router } from "express";
 import { Response, Request } from "express"
 import { protect } from "../middleware/auth.middleware.ts";
 import { updateProfile } from "../controllers/user.controller.ts";
-import { checkUsername } from "../controllers/user.controller.ts";
+import { checkUsername, searchUsers } from "../controllers/user.controller.ts";
 
 const userRouter = Router();
 
-userRouter.patch("/update-profile", protect, updateProfile);
-userRouter.post("/check-username", protect, checkUsername);
+userRouter.use(protect);
+
+userRouter.patch("/update-profile", updateProfile);
+userRouter.post("/check-username", checkUsername);
+
+userRouter.get("/search", searchUsers);
 
 export default userRouter;
