813d5fb9576fbeb1aa2fe9e3848c276ed422dd6f
JosefStack <jojohnson350@gmail.com>
2026-04-14 15:36:27 +0300

    change from cookie to passing token via auth object

--- server/src/middleware/socket.middleware.ts
+++ server/src/middleware/socket.middleware.ts
@@ -1,15 +1,16 @@
 import { Socket } from "socket.io";
-import jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import { prisma } from "../lib/prisma.js";
 
 
 export const socketAuthMiddleware = async (socket: Socket, next: (err?: Error) => void) => {
 
     try {
-        const token = socket.handshake.headers.cookie
-        ?.split("; ")
-        .find((row) => row.startsWith("jwt="))
-        ?.split("=")[1];
+        const token = socket.handshake.auth?.token ||
+            socket.handshake.headers.cookie
+                ?.split("; ")
+                .find((row) => row.startsWith("jwt="))
+                ?.split("=")[1];
 
         if (!token) {
             console.log("Socket rejected: no token");
@@ -19,8 +20,8 @@ export const socketAuthMiddleware = async (socket: Socket, next: (err?: Error) =
         const decoded = jwt.verify(token, process.env.JWT_SECRET) as { userId: string };
 
         const user = await prisma.user.findUnique({
-            where: {id: decoded.userId},
-            select: {id: true, name: true, username: true, avatar: true}
+            where: { id: decoded.userId },
+            select: { id: true, name: true, username: true, avatar: true }
         })
 
         if (!user) {
@@ -31,7 +32,7 @@ export const socketAuthMiddleware = async (socket: Socket, next: (err?: Error) =
         socket.data.user = user;
         console.log(`Socket authenticated: ${user.username}`);
         next();
-    
+
     } catch (err) {
         console.log("Error in socket auth: ", err);
         next(new Error("Unauthorized"));
