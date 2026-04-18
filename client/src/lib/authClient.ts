45f89abeeb46bd69d0e3bff2644d78f11431d466
JosefStack <jojohnson350@gmail.com>
2026-04-18 21:45:48 +0300

    updaed authClient baseURL for production

--- client/src/lib/authClient.ts
+++ client/src/lib/authClient.ts
@@ -3,5 +3,5 @@ import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
     baseURL: import.meta.env.DEV 
         ? "http://localhost:3000"
-        : "",
+        : import.meta.env.VITE_API_URL.replace("/api", ""),
 }); 
