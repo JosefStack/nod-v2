79aa0863c03f093746b1e37a478a63f407d24c13
JosefStack <jojohnson350@gmail.com>
2026-05-10 19:15:50 +0300

    added health route to nod backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -46,6 +46,10 @@ app.use('/api/message', messageRouter);
 app.all('/api/auth/{*any}', toNodeHandler(auth));
 
 
+app.get('/health', (req, res) => {
+    res.status(200).json({"status" : "ok"})
+})
+
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
