5f7e6b60c7867ea951b4b0713e4dc495f30f99bb
JosefStack <jojohnson350@gmail.com>
2026-05-14 11:59:33 +0300

    remove test cases from reranker

--- services/ai/app/utils/rerank.py
+++ services/ai/app/utils/rerank.py
@@ -10,16 +10,3 @@ client = voyageai.AsyncClient(api_key=os.getenv("VOYAGE_API_KEY"))
 async def rerank(query, chunks):
     ranks = await client.rerank(query, chunks, model="rerank-2.5")
     return [(rank.index, rank.relevance_score) for rank in ranks.results]
-
-
-async def main():
-    query = "what is my name?"
-    chunks = [
-        "my name is bek",
-        "his name is keb"
-    ]
-
-    await rerank(query, chunks)
-
-
-asyncio.run(main())
